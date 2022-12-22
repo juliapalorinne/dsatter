0ff538cd8cfe77bb890d58c0777362ed161874a1
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2022-12-22 21:39:38 +0200

    client: initial commit. Sends messages to server. Handles, draws new messages from server.

---
+++ node-client/logic/message_handler.py
@@ -0,0 +1,237 @@
+import logging
+import threading
+import queue
+import datetime
+import uuid # id = uuid.uuid4()
+import json
+from typing import Union, List
+
+
+class Message:
+    def __init__(self, message: dict) -> 'Message':
+        missing_fields = self.__validate_fields(message)
+        if len(missing_fields) > 0:
+            raise ValueError("Required fields: '" + "', '".join(missing_fields) + "' are missing from the new message")
+
+        self.__message = self.__parse_message(message)
+        if len(self.__message.keys()) == 0:
+            raise ValueError('Error while parsing the new message')
+
+
+    @property
+    def message(self) -> dict:
+        return self.__message
+
+
+    def __lt__(self, other: 'Message') -> bool:
+        return self.__message['dateTime'] < other.__message['dateTime']
+
+
+    def __parse_message(self, message: dict) -> dict:
+        parsed_msg = message
+        error = False
+
+        if not isinstance(parsed_msg['messageId'], str):
+            logging.warning('parsing new message, messageId is not of type str')
+            error = True
+        else: # NOTE: This hack can be removed when the node-server is fixed to return messageId as ints
+            try:
+                parsed_msg['messageId'] = int(parsed_msg['messageId'])
+            except Exception as err:
+                logging.warning('parsing new message, messageId cannot be casted to int')
+                error = True
+        if not isinstance(parsed_msg['text'], str):
+            logging.warning('parsing new message, text is not of type str')
+            error = True
+        if not isinstance(parsed_msg['dateTime'], str):
+            logging.warning('parsing new message, dateTime is not of type str')
+            error = True
+        else:
+            try:
+                parsed_msg['dateTime'] = datetime.datetime.strptime(
+                    parsed_msg['dateTime'],
+                    '%Y-%m-%dT%H:%M:%S.%fZ'
+                )
+            except Exception as err:
+                logging.warning('Parsing new message, dateTime has invalid format:', err)
+                error = True
+        if not isinstance(parsed_msg['sender'], str):
+            logging.warning('parsing new message, sender is not of type str')
+            error = True
+
+        if not isinstance(parsed_msg['chatId'], int):
+            logging.warning('Parsing new message, chatId is not of type int')
+            error = True
+
+        return parsed_msg if not error else {}
+
+
+    def __validate_fields(self, message: dict) -> List[str]:
+        # TODO: Check that no additional fields are set (?)
+        fields_expected = ['messageId', 'text', 'dateTime', 'sender', 'chatId']
+        # TODO: nodeId, id <- fields should be deleted in node-server before broadcasting msg to client
+
+        fields_missing = []
+
+        for f in fields_expected:
+            if not f in message.keys():
+                fields_missing.append(f)
+
+        return fields_missing
+
+
+    def __str__(self) -> str:
+        return 'Message:\n' + '\n'.join([f'{k}: {v}' for k,v in self.__message.items()])
+
+
+class MessageHandler(threading.Thread):
+    '''
+    A class that extends the threading.Thread class. The thread is NOT run
+    as a daemon.
+    '''
+
+    Websocket_msg_sender = None
+
+    def __init__(self) -> 'MessageHandler':
+        super().__init__()
+
+        self.__MSG_TYPES = {
+        'clientSyncRequest': None,
+        'clientSyncReply': None,
+        'newMessageFromClient': None,
+        'newMessagesForClient': self.__handle_new_msg,
+        'clientMessageResponse': lambda added: print('RESPONSE (added):', added)
+        }
+
+        self.__message_queue = queue.Queue()
+        self.__on_message_event: callable = None
+        self.__continue = True
+
+
+    def handle_new_client_message(self, message: str):
+        # TODO: Use queue for outgoing messages
+
+        msg = {
+            'type': 'newMessageFromClient',
+            'payload': {
+                'text': message,
+                'sender': 'pythonClientUser',
+                'chatId': 11
+            }
+        }
+
+        if MessageHandler.Websocket_msg_sender is None:
+            logging.info('WS message sender not installed, discarding new message')
+            return
+
+        MessageHandler.Websocket_msg_sender(json.dumps(msg))
+
+
+    def handle_incoming(self, message: str) -> None:
+        try:
+            msg = json.loads(message)
+        except Exception as err:
+            logging.error(err)
+            logging.error('Discarding incoming message')
+            return
+
+        if not ('type' in msg and 'payload' in msg):
+            logging.error('Received incomplete message:', msg)
+            return
+
+        if msg['type'] in self.__MSG_TYPES:
+            for m in msg['payload']:
+                self.__MSG_TYPES[msg['type']](m)
+        else:
+            logging.info(f'RECEIVED unknown message type, ignoring: {msg}')
+
+
+    def __handle_new_msg(self, message: dict):
+        try:
+            msg = Message(message)
+        except Exception as err:
+            logging.warning(f'Error when handling incoming message: {err}')
+
+        # TODO: Use try/except to catch case if queue is full
+        self.__message_queue.put(msg, block=False)
+
+
+    @property
+    def on_message_event(self) -> callable:
+        return self.__on_message_event
+
+
+    @on_message_event.setter
+    def on_message_event(self, func: callable) -> None:
+        logging.debug('Message handler installed')
+        self.__on_message_event = func
+
+
+    def run(self) -> None:
+        '''
+        This method is called by threading.Thread.start method.
+        The 'main' function of the thread.
+        '''
+
+        logging.debug('Message handler thread initialized')
+
+        while self.__continue:
+            self.__continue = self.handle_message_event()
+
+        logging.debug('Message handler thread stopping')
+
+
+    def terminate(self) -> None:
+        self.__message_queue.put(None, block=False)
+#    def create_message(self, message: Union[str, None]) -> None:
+#        '''
+#        Append a new message to the queue. This thread will stop
+#        if message is None. This function never blocks, only logs
+#        error messages if there is any exceptions when attempting to
+#        add messages to the queue.
+#        '''
+#
+#        if message is not None:
+#            logging.debug(f'Creating message: {message}')
+#            if len(message) == 0:
+#                return
+#
+#            ordered_msg = OrderedMessage({
+#                'id': uuid.uuid4(),
+#                'timestamp': datetime.datetime.now(),
+#                'sender': 'Test Tester',
+#                'message': message
+#            })
+#
+#            logging.debug(f'ordered message: {ordered_msg.message["message"]}')
+#
+#        try:
+#            self.__message_queue.put(
+#                ordered_msg if message is not None else None,
+#                block=False
+#            )
+#        except queue.Full as e:
+#            logging.error('Messages queue is full, not handeled:', e)
+#        except Exception as e:
+#            logging.error('Exception happened when attempting to add message to the queue: ', e)
+
+
+    def handle_message_event(self) -> bool:
+        msg = self.__message_queue.get()
+        if msg is None:
+            return False
+
+
+        #self.__messages.put(msg)
+
+        # NOTE: Python priority queues are implemented as binary heap data structure,
+        #       so printing the raw elements in the internal queue does not follow the
+        #       ordering in the priority queue.
+        #logging.debug(f'messages {[ m.message for m in self.__messages.queue ]}')
+
+        if self.__on_message_event is None:
+            return
+
+        self.__on_message_event([ msg.message ])
+
+        return True
---
+++ node-client/ui/gui.py
@@ -0,0 +1,250 @@
+import logging
+import tkinter as tk
+
+from datetime import datetime
+from tkinter import ttk
+from typing  import Tuple
+
+
+#class ReadOnlyText(tk.Text):
+#    def __init__(self, *args, **kwargs):
+#        tk.Text.__init__(self, *args, **kwargs)
+#        self.bind('<Key>', lambda e: ReadOnlyText.filter_txt_events(e))
+#
+#    @staticmethod
+#    def filter_txt_events(event):
+#        '''
+#        Kills the event if it's not caused by a ctrl+c.
+#        '''
+#        # value of state is apparently different on windows hosts(?),
+#        # NOT tested for.
+#        ctrl_pressed_state = 12 if os.name == 'nt' else 4
+#        if event.state == ctrl_pressed_state and event.keysym == 'c':
+#            return
+#        return "break"
+
+
+class GridFrame(ttk.Frame):
+    def __init__(self, master, grid_row, grid_column, grid_columnspan):
+        super().__init__(master)
+        self.grid(row=grid_row, column=grid_column, columnspan=grid_columnspan, pady=1, padx=1)
+
+
+class StatusFrame(GridFrame):
+    def __init__(self, master, grid_row, grid_column, grid_columnspan):
+        super().__init__(master, grid_row, grid_column, grid_columnspan)
+
+        self.__node_label = ttk.Label(self, text='NOT IMPLEMENTED: Connected to node:')
+        self.__node_label.grid(row=0, column=0, pady=1, padx=1)
+
+        self.__node_info = ttk.Label(self, text='127.0.0.1:8080')
+        self.__node_info.grid(row=0, column=1, pady=1, padx=1)
+
+        self.__user_label = ttk.Label(self, text='NOT IMPLEMENTED: Name')
+        self.__user_label.grid(row=1, column=0, pady=1, padx=1)
+
+        self.__user_entry = ttk.Entry(self, text='Dsatter user')
+        self.__user_entry.grid(row=1, column=1, pady=1, padx=1)
+
+        self.__user_set_btn = ttk.Button(self, text='Set username')
+        self.__user_set_btn.grid(row=1, column=2, pady=1, padx=1)
+
+
+#class MessagesFrame(GridFrame):
+#    def __init__(self, master, grid_row, grid_column, grid_columnspan):
+#        super().__init__(master, grid_row, grid_column, grid_columnspan)
+#
+#        self.__messages = ReadOnlyText(self)
+#        self.__messages.pack(side=tk.LEFT, fill=tk.BOTH)
+#        self.__messages.insert(tk.END, 'No messages.. :(')
+#
+#        self.__scrollbar = ttk.Scrollbar(self)
+#        self.__scrollbar.pack(side=tk.RIGHT, fill=tk.BOTH)
+#        self.__messages.config(yscrollcommand=self.__scrollbar.set)
+#        self.__scrollbar.config(command=self.__messages.yview)
+#
+#    def _add_new_message(self, message: OrderedMessage) -> None:
+#        def formatter(m: OrderedMessage):
+#            return f'\n{m["dateTime"].strftime("%H:%M")} - {m["sender"]}: {m["text"]}'
+#        self.__messages.insert(tk.END, formatter(message))
+#        print('MessageFrame\n--------------\n', message, '\n')
+
+
+class MessagesFrame(GridFrame):
+    TIME_STR_F = '%Y-%m-%dT%H:%M:%S.%fZ'
+
+    def __init__(self, master, grid_row, grid_column, grid_columnspan):
+        super().__init__(master, grid_row, grid_column, grid_columnspan)
+        self.__message_ids = set()
+        self.__init_view()
+
+
+    def __init_view(self):
+        columns = ('dateTime', 'sender', 'text')
+        self.__tree = ttk.Treeview(self, columns=columns, show='headings')
+        self.__tree.heading('dateTime', text='Time')
+        self.__tree.heading('sender', text='Sender')
+        self.__tree.heading('text', text='Message text')
+        self.__tree.pack(side=tk.LEFT, fill=tk.BOTH)
+
+        self.__tree.bind('<<TreeviewSelect>>', self.__show_selected)
+
+        self.__scrollbar = ttk.Scrollbar(self, orient=tk.VERTICAL, command=self.__tree.yview)
+        self.__tree.configure(yscroll=self.__scrollbar.set)
+        self.__scrollbar.pack(side=tk.RIGHT, fill=tk.BOTH) #grid(row=0, column=1, sticky='ns')
+
+    def __show_selected(self, event):
+        for selected_msg in self.__tree.selection():
+            item = self.__tree.item(selected_msg)
+            print('\nselected:', selected_msg)
+            print('item:', item)
+            print('-----\n')
+
+
+    def __insert_message(self, msg):
+        def rendered_msg_ts(iid: str) -> datetime:
+            return datetime.strptime(iid.split(' ')[1], MessagesFrame.TIME_STR_F)
+
+        def insert_into_tree(indx: int) -> str:
+            return self.__tree.insert(
+                parent='',
+                index=indx,
+                iid=(msg['messageId'], msg['dateTime'].strftime(MessagesFrame.TIME_STR_F)),
+                values=(msg['dateTime'].strftime('%d.%m %H:%M.%S'), msg['sender'], msg['text'])
+            )
+
+        if msg['messageId'] in self.__message_ids:
+            print('Message is already rendered. Editing or updating edited messages is not supported')
+            return
+
+        children = self.__tree.get_children()
+
+        # Select position for inserting
+        if len(children) == 0 or \
+           msg['dateTime'] < rendered_msg_ts(children[0]):
+            i = 0
+        elif msg['dateTime'] >= rendered_msg_ts(children[-1]):
+            i = len(children)
+        else: # Do binary search to set i to equal the position where all the previous rendered messages
+              # timestamps preceeds the msg's timestamp.
+            n = len(children)
+            i = 0
+            j = n
+            while True:
+                j //= 2
+                if j < 1: break
+                while i+j < n:
+                    if rendered_msg_ts(children[i+j]) <= msg['dateTime']: i += j
+                    else: break
+            # i points to the last rendered message whos timestamp is less than or equal to the new msgs
+            i += 1 # advance i to next pos
+
+        if i > len(children):
+            print('binary search error... message not inserted')
+            return
+        elif i == len(children):
+            i = tk.END
+        else:
+            i = self.__tree.index(children[i])
+
+        # Update status
+        insert_into_tree(i)
+        self.__message_ids.add(msg['messageId'])
+
+
+    def _add_messages(self, msgs):
+        for m in msgs:
+            self.__insert_message(m)
+
+
+class MessageEntryFrame(GridFrame):
+    def __init__(self, master, grid_row, grid_column, grid_columnspan, new_msg_handler: callable):
+        super().__init__(master, grid_row, grid_column, grid_columnspan)
+
+        self.__new_msg_handler = new_msg_handler
+
+        self.__label_msg = ttk.Label(self, text='Message:')
+        self.__label_msg.grid(row=0, column=0, pady=1, padx=1)
+
+        self.__entry = ttk.Entry(self, width=56)
+        self.__entry.grid(row=0, column=1, pady=1, padx=1)
+        self.__entry.insert(0, 'Message..')
+        self.__entry.configure(state=tk.DISABLED)
+        self.__entry.bind('<Return>', self.__on_new_message)
+        self.__entry.bind('<Button-1>', self.__on_entry_click)
+
+        self.__button_send = ttk.Button(self, text='Send', command=self.__on_new_message)
+        self.__button_send.grid(row=0, column=2, pady=1, padx=1)
+
+    def __on_entry_click(self, event):
+        self.__entry.configure(state=tk.NORMAL)
+        self.__entry.delete(0, tk.END)
+        self.__entry.unbind('<Button-1>')
+
+    def __on_new_message(self, key_event = None):
+        message = self.__entry.get()
+        if len(message) == 0:
+            return
+
+        if self.__new_msg_handler == None:
+            logging.error('MessageEntryFrame: Discarding new message, no handler installed')
+            return
+
+        self.__new_msg_handler(message)
+        self.__entry.delete(0, len(message))
+
+
+class App(ttk.Frame):
+    def __init__(
+            self,
+            win_title: str,
+            new_client_msg_handler,
+            win_maxsize: Tuple[int, int] = (1024, 1024),
+            master: tk.Tk = None) -> 'App':
+        super().__init__(master)
+        self.master.title(win_title)
+        #self.master.minsize(width, height)
+        self.master.maxsize(win_maxsize[0], win_maxsize[1])
+
+        self.__new_client_msg_handler = new_client_msg_handler
+
+        self.pack(pady=1, padx=1)
+
+        self.__init_menu()
+
+        self.__status_frame    = StatusFrame(self, grid_row=0, grid_column=0, grid_columnspan=3)
+        self.__messages_frame  = MessagesFrame(self, grid_row=1, grid_column=0, grid_columnspan=3)
+        self.__msg_entry_frame = MessageEntryFrame(self, grid_row=2, grid_column=0, grid_columnspan=3, new_msg_handler=self.__new_client_msg_handler)
+
+    def __init_menu(self):
+        self.__menu = tk.Menu(self.master)
+        self.master.config(menu=self.__menu)
+
+        self.__menuFile = tk.Menu(self.__menu)
+        self.__menuFile.add_command(label='Exit', command=self.__quit)
+        self.__menu.add_cascade(label='File', menu=self.__menuFile)
+
+        self.__menuEdit = tk.Menu(self.__menu)
+        self.__menu.add_cascade(label='Edit', menu=self.__menuEdit)
+
+    def __quit(self):
+        print('k thx goodbye')
+        self.master.quit()
+
+    def refresh_msgs(self, messages: list) -> None:
+        logging.debug('New Message!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+
+        print('MESSAGE LIST:', messages)
+        self.__messages_frame._add_messages(messages)
+
+#    def close_window(self):
+#        pass
+#
+#    def close(self):
+#        pass
+
+
+if __name__ == '__main__':
+    pass
+    #app = App(tk.Tk())
+    #app.master.mainloop()
